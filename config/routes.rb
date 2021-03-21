ba9942b3b2f1d6290c46a129cbb82f0bb30b49f4
斎藤正大 <saima.ptw88@gmail.com>
2021-03-21 21:20:46 +0900

    retouching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controller: {
-    registrations: "auth/registrations"
+  mount_devise_token_auth_for "User", at: "auth", controller: {
+    registrations: "auth/registrations",
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
