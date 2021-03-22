28265b0b837ae4e56fc32d93532f5f68037e8929
斎藤正大 <saima.ptw88@gmail.com>
2021-03-22 12:36:30 +0900

    r

--- spec/models/want_spec.rb
+++ spec/models/want_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Want, type: :model do
       @user = create(:user)
       @goal = create(:goal, user_id: @user.id)
     end
+
     let(:want) { create(:want, user_id: @user.id, goal_id: @goal.id) }
 
     it "success" do
