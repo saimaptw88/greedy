63151629917279529e057f6e1669fc36cfaab4b9
斎藤正大 <saima.ptw88@gmail.com>
2021-03-23 07:49:59 +0900

    add show method to want_controller

--- app/controllers/api/v1/want_controller.rb
+++ app/controllers/api/v1/want_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::WantController < ApplicationController
+  before_action :set_params, only: [:show, :update, :destroy]
   def index
     @wants = current_user.wants.order(priority: :asc)
     render json: @wants, each_serializer: Api::V1::PreviewWantSerializer
   end
+
+  def show
+    render json: @want, serializer: Api::V1::PreviewWantSerializer
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def set_params
+      @want = current_user.wants.find(params[:id])
+    end
 end
