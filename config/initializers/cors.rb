f1fb2f48c89af2be18a38a6968813fbed832ec8c
斎藤正大 <saima.ptw88@gmail.com>
2021-03-29 20:09:01 +0900

    retouching

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "http://localhost:8080"
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Per-Page', 'Total', 'Link']
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             expose: ["Per-Page", "Total", "Link"]
   end
 end
