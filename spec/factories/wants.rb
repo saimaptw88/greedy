8fdf3b5c2096f43d7f8557bcd3fb88b78ba0e2fe
斎藤正大 <saima.ptw88@gmail.com>
2021-04-03 17:07:34 +0900

    retouching want spec

--- spec/factories/wants.rb
+++ spec/factories/wants.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :want do
-    target { "To create #{Faker::App.name}" }
+    array = [1, 2]
+    name { "To create #{Faker::App.name}" }
     priority { Faker::Number.decimal_part(digits: 1) }
     reachability { Faker::Number.decimal_part(digits: 2) }
+    category_id { array.sample }
 
     user
   end
