53077bbabab90c998ee6c7506afdef3008e41bb4
斎藤正大 <saima.ptw88@gmail.com>
2021-04-11 17:03:28 +0900

    retouching want_controller and model to attach priority

--- spec/factories/wants.rb
+++ spec/factories/wants.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :want do
     array = [1, 2]
-    # num = 0
     name { "To create #{Faker::App.name}" }
-    priority { Faker::Number.decimal_part(digits: 1) }
+    sequence(:priority) {|i| i - 1 }
     reachability { Faker::Number.decimal_part(digits: 2) }
     category_id { array.sample }
 
