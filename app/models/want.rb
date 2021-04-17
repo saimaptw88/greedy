5b0ceb096be0ca6d6ca75aa30e8611e232f6165f
斎藤正大 <saima.ptw88@gmail.com>
2021-04-17 10:19:05 +0900

    retouch updates function to update wants simultanously

--- app/models/want.rb
+++ app/models/want.rb
@@ -1,3 +1,10 @@
 class Want < ApplicationRecord
   belongs_to :user
+
+  # def want.malti_update(wants_params)
+  #   wants_params.map do |id, want_params|
+  #     want = current_user.wants.find(id)
+  #     want.update_attributes(want_params)
+  #   end
+  # end
 end
--- spec/factories/wants.rb
+++ spec/factories/wants.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :want do
-    array = [1, 2]
+    # array = [1, 2]
+    array = [1]
+    sequence(:id) {|i| i - 1 }
     name { "To create #{Faker::App.name}" }
+    category_id { array.sample }
     sequence(:priority) {|i| i - 1 }
     reachability { Faker::Number.decimal_part(digits: 2) }
-    category_id { array.sample }
 
     user
   end
