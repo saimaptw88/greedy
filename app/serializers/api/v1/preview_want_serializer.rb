daaa531cb41fe42d16c4e98b36425703da79487d
斎藤正大 <saima.ptw88@gmail.com>
2021-05-15 16:55:49 +0900

    retouching serializer

--- app/serializers/api/v1/preview_want_serializer.rb
+++ app/serializers/api/v1/preview_want_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::PreviewWantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :priority, :reachability, :category_id
+  attributes :id, :name, :priority, :reachability, :category_id, :why, :every_day_task
 
   belongs_to :user
 end
